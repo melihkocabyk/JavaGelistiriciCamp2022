2b127bc04e4a31406b103465fa1c5fd1df9e36d0
Melih Kocabıyık <92519969+melihkocabyk@users.noreply.github.com>
2022-11-10 21:36:08 +0200

    Delete dayFour directory

--- dayFour/Homework-1/kodlama.io.Devs/src/main/java/kodlama/io/kodlama/io/Devs/dataAccess/abstracts/ProgrammingLanguageRepository.java
+++
@@ -1,9 +0,0 @@
-package kodlama.io.kodlama.io.Devs.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlama.io.kodlama.io.Devs.entities.concretes.ProgrammingLanguage;
-
-public interface ProgrammingLanguageRepository extends JpaRepository<ProgrammingLanguage, Integer>{
-
-}
